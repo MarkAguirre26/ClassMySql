b0c1187caa5cfe844831971245476ff74f2ca42a
MarkAguirre26 <markaguirre26@gmail.com>
2016-11-28 05:58:16 -0800

    Add files via upload

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd_Command = new System.Windows.Forms.Button();
+            this.cmd_get = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmd_Command
+            // 
+            this.cmd_Command.Location = new System.Drawing.Point(12, 12);
+            this.cmd_Command.Name = "cmd_Command";
+            this.cmd_Command.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Command.TabIndex = 0;
+            this.cmd_Command.Text = "Insert";
+            this.cmd_Command.UseVisualStyleBackColor = true;
+            this.cmd_Command.Click += new System.EventHandler(this.cmd_Command_Click);
+            // 
+            // cmd_get
+            // 
+            this.cmd_get.Location = new System.Drawing.Point(12, 41);
+            this.cmd_get.Name = "cmd_get";
+            this.cmd_get.Size = new System.Drawing.Size(75, 23);
+            this.cmd_get.TabIndex = 1;
+            this.cmd_get.Text = "Read";
+            this.cmd_get.UseVisualStyleBackColor = true;
+            this.cmd_get.Click += new System.EventHandler(this.cmd_get_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(350, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 220);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmd_get);
+            this.Controls.Add(this.cmd_Command);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd_Command;
+        private System.Windows.Forms.Button cmd_get;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassMysql;
+
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClassMysql.ClassMysql.Constring = "Server=localhost;Database=etop;Uid=root;Pwd=;";
+           
+        }
+        private void cmd_Command_Click(object sender, EventArgs e)
+        {
+            ClassMysql.ClassMysql a = new ClassMysql.ClassMysql();
+            a.ExecuteQuery("INSERT INTO `tbl_user` (`FullName`, `Username`, `Password`, `UserLevel`) VALUES ('k', 'k', 'k', '0')");
+        }
+
+        private void cmd_get_Click(object sender, EventArgs e)
+        {
+            ClassMysql.ClassMysql a = new ClassMysql.ClassMysql(); DataTable dt;
+            dt = a.Table("Select * from tbl_user");
+           dataGridView1.Rows.Clear();
+           foreach (DataRow dr in dt.Rows)
+           {
+               dataGridView1.Rows.Add(dr[0].ToString(), dr[1].ToString(), dr[2].ToString());
+           }
+        }
+    }
+}
